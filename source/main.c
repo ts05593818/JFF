c65cb53a9794812cff3f8be8dfc1d8cc1fe0bcf8
Yen,Shih-Min <s9001922000@gmail.com>
2020-01-14 23:25:44 +0800

    clear uart print test flow

--- source/main.c
+++ source/main.c
@@ -1,34 +1,7 @@
 #include "..\header\main.h"
-int main(){
-   
-    
+int main() {
     uart_init();
-
-    int  data=1;
-    char cc='e';
-    char *str="abcde";
-    RCC_AHB1_ENR |= 1<<3; //enable gpiod
-    GPIO_MODER(GPIOD_BASE) |= 1<<24; // pd 12~15
-    GPIO_OTYPER(GPIOD_BASE) = 0;
-    GPIO_OSPEEDR(GPIOD_BASE) = 1<<24;
-    GPIO_PUPDR(GPIOD_BASE) = 0;
-    GPIO_ODR(GPIOD_BASE) |= 1<<12;
-
-    while (1)
-    {
-    uart_print("data=%d",data);
-    uart_print("char=%c",cc);
-    uart_print("string=%s",str);
-    uart_print("hex=%x",data);
-    uart_print("data=%d char=%c string=%s hex=%x",data,cc,str,data);
-    data=(data+1)%20;
-    GPIO_ODR(GPIOD_BASE) ^= 1<<12;
-    for (int i=0;i<1000000;i++) {;}
-    GPIO_ODR(GPIOD_BASE) ^= 1<<12;
-    for (int i=0;i<1000000;i++) {;}
-
+    while (1) {
+          ;
     }
-    
-
 }
-
--- source/startup.c
+++ source/startup.c
@@ -6,39 +6,44 @@ extern uint32_t _enddata;
 extern uint32_t _startbss;
 extern uint32_t _endbss;
 extern uint32_t _estack;
-void init_core_clk(void){
-    RCC_CR |= PLL_ON|(1<<16)|(1<<19);
-    while((!(RCC_CR&(1<<17)))||(!(RCC_CR&(1<<25))));
-    RCC_PLLCFGR =(1<<22)|(PLLM(5)) | (PLLN(60)) | (PLLQ(2))|(PLLP(2));
+void init_core_clk(void) {
+    RCC_CR |= PLL_ON | (1 << 16) | (1 << 19);
+    while ((!(RCC_CR & (1 << 17))) || (!(RCC_CR & (1 << 25))))
+        ;
+    RCC_PLLCFGR = (1 << 22) | (PLLM(5)) | (PLLN(60)) | (PLLQ(2)) | (PLLP(2));
     RCC_CFGR = 1;
 }
 
 extern int main(void);
 void reset_handler(void) {
-    uint32_t* end_text = &_endtext;
-    uint32_t* start_data = &_startdata;
-    uint32_t* end_data = &_enddata;
-    while(start_data<end_data) *start_data++=*end_text++;
-    uint32_t* start_bss = &_startbss;
-    uint32_t* endbss = &_endbss;
-    while(start_bss<endbss)*start_bss++=0;
+    uint32_t *end_text = &_endtext;
+    uint32_t *start_data = &_startdata;
+    uint32_t *end_data = &_enddata;
+    while (start_data < end_data) *start_data++ = *end_text++;
+    uint32_t *start_bss = &_startbss;
+    uint32_t *endbss = &_endbss;
+    while (start_bss < endbss) *start_bss++ = 0;
     init_core_clk();
     main();
 }
-void nmi_handler (void){
-    while(1);
+void nmi_handler(void) {
+    while (1)
+        ;
 }
-void hard_fault_handler(void){
-    while(1);
+void hard_fault_handler(void) {
+    while (1)
+        ;
 }
-void mem_manage_handler(void){
-    while(1);
+void mem_manage_handler(void) {
+    while (1)
+        ;
 }
-void bus_fault_handler(void){
-    while (1);       
+void bus_fault_handler(void) {
+    while (1)
+        ;
 }
 __attribute((section(".isr_vector"))) uint32_t *isr_vectors[] = {
-    (uint32_t *)&_estack, 
+    (uint32_t *)&_estack,
     (uint32_t *)reset_handler, /* code entry point */
     (uint32_t *)nmi_handler,
     (uint32_t *)hard_fault_handler,
